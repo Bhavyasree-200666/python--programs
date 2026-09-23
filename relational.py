fdcc53ddcc672b85b013a1c166e847c7fe65fb02
Bhavyasree-200666 <oletibhavyasree625@gmail.com>
2026-09-23 20:29:58 +0530

    Add files via upload

---
+++ relational.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a the first number:"))
+b=int(input("Enter a second value:"))
+print("a==b:",a==b)
+print("a=!b:",a!=b)
+print("a>=b:",a>=b)
+print("a<=b:",a<=b)
+print("a<b:",a<b)
+print(a>b)
+      
